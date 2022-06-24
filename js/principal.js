58ed82c5ec091e6d924910e066269edae36c2a13
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-06-24 18:54:53 -0300

    Acessando e manipulando o HTML atraves do DOM

--- js/principal.js
+++ js/principal.js
@@ -3,3 +3,21 @@ console.log("Fui carregado de um arquivo externo");
 var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Aparecida Nutricionista";
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+
+// conteúdo da tag td
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+//formula do IMC
+var imc = peso / (altura * altura);
+
+//acessando a coluna do IMC no HTML
+var tdImc = paciente.querySelector(".info-imc");
+
+//inserindo o valor calculado dentro de .info-imc
+tdImc.textContent = imc;
