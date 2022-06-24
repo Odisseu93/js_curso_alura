6186521c72ba9fb32d255c5f9b2911102c380eb4
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-06-24 19:13:06 -0300

    Validando os dados do usuário

--- js/principal.js
+++ js/principal.js
@@ -14,10 +14,35 @@ var peso = tdPeso.textContent;
 var altura = tdAltura.textContent;
 
 //formula do IMC
-var imc = peso / (altura * altura);
+// var imc = peso / (altura * altura);
 
 //acessando a coluna do IMC no HTML
-var tdImc = paciente.querySelector(".info-imc");
+//var tdImc = paciente.querySelector(".info-imc");
 
 //inserindo o valor calculado dentro de .info-imc
-tdImc.textContent = imc;
+// tdImc.textContent = imc;
+
+// peso = 9000;
+
+
+// Validação de dados
+var tdImc = paciente.querySelector(".info-imc");
+var alturaEhValida = true;
+var pesoEhValido = true;
+
+if(peso <= 0 || peso > 1000){
+    console.log("Peso inválido");
+    tdImc.textContent = "Peso inválido!";
+    pesoEhValido = false;
+}
+
+if(altura <= 0 || altura >= 3){
+    console.log("Altura inválida");
+    tdImc.textContent = "Altura inválida!";
+    alturaEhValida = false;
+}
+
+if(pesoEhValido && alturaEhValida){
+    var imc = peso / ( altura * altura);    
+    tdImc.textContent = imc;
+}
