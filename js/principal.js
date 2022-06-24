0be563d4e5ab04956481f4a927e310c77cf5adde
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-06-24 19:16:48 -0300

    adicionado o else no final da validação pelos ifs encadeados

--- js/principal.js
+++ js/principal.js
@@ -30,19 +30,21 @@ var tdImc = paciente.querySelector(".info-imc");
 var alturaEhValida = true;
 var pesoEhValido = true;
 
-if(peso <= 0 || peso > 1000){
-    console.log("Peso inválido");
-    tdImc.textContent = "Peso inválido!";
-    pesoEhValido = false;
+if (peso <= 0 || peso > 1000) {
+  console.log("Peso inválido");
+  tdImc.textContent = "Peso inválido!";
+  pesoEhValido = false;
 }
 
-if(altura <= 0 || altura >= 3){
-    console.log("Altura inválida");
-    tdImc.textContent = "Altura inválida!";
-    alturaEhValida = false;
+if (altura <= 0 || altura >= 3) {
+  console.log("Altura inválida");
+  tdImc.textContent = "Altura inválida!";
+  alturaEhValida = false;
 }
 
-if(pesoEhValido && alturaEhValida){
-    var imc = peso / ( altura * altura);    
-    tdImc.textContent = imc;
-}
+if (pesoEhValido && alturaEhValida) {
+  var imc = peso / (altura * altura);
+  tdImc.textContent = imc;
+} else {
+  tdImc.textContent = "Altura e/ou peso inválidos!"
+}  
