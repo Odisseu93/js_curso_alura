4bbbeccf913dfc3f3535a1146cf2a83f1a76a853
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-06-29 21:23:35 -0300

    removendo itens da tabela

---
+++ js/remover_paciente.js
@@ -0,0 +1,11 @@
+var tabela = document.querySelector("table");
+
+
+// event.target é aonde foi clicado, enquanto parentNode é o seu  pai
+tabela.addEventListener("dblclick", (event) => {
+  event.target.parentNode.classList.add("fadeOut"); // adicionando uma animação via classe no CSS
+  setTimeout(function () { //define um tempo para executar uma função
+    event.target.parentNode.remove();
+  }, 500)
+});
+
